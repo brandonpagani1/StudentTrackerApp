8982911480bd625cfab0bcdb1e5b5130e74cb68e
brandonpagani1 <brandon_pagani@hotmail.com>
2026-05-21 12:01:14 -0400

    Update announcements.js

--- server/models/announcements.js
+++ server/models/announcements.js
@@ -1,1 +1,19 @@
+const mongoose = require("mongoose");
 
+const announcementSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  message: {
+    type: String,
+    required: true
+  },
+  course: String,
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Announcement", announcementSchema);
