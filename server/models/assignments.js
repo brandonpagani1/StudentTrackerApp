3e02f0702e6612f0f4ddc19cda26c4c40f0c5aae
brandonpagani1 <brandon_pagani@hotmail.com>
2026-05-21 12:02:05 -0400

    Update assignments.js

--- server/models/assignments.js
+++ server/models/assignments.js
@@ -1,1 +1,35 @@
+const mongoose = require("mongoose");
 
+const assignmentSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, "Title is required"]
+  },
+  course: {
+    type: String,
+    required: true
+  },
+  dueDate: {
+    type: Date,
+    required: true,
+    index: true 
+  },
+  grade: {
+    type: Number,
+    min: 0,
+    max: 100
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    index: true
+  },
+  status: {
+    type: String,
+    enum: ["pending", "completed"],
+    default: "pending"
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Assignment", assignmentSchema);
